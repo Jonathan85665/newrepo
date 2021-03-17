8b4867f1e5c501f376d0536f2768b4c22642a103
unknown <you@example.com>
2021-03-17 18:26:09 -0500

    making some changes

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
 		System.out.println("Adding one more line");
+		System.out.println("Making some changes");
 
 	}
 
